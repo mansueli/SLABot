3d85299c08f5c33e8d8f4745f38e64134bab744e
Rodrigo Mansueli Nunes <mansueli@engineer.com>
2016-07-31 23:51:26 -0300

    1.0.6
    **FIXED**
    Icon that wasn't working well on previous commits. 
    (Getting icon as a resource now) 
    **ADDED**
    Unix Support
    
    **TESTING**
    Tracking indefinitely ( no longer necessary to update manually the tracking sheet once the second week was completed)
    Added template as resource for Unix / Mac OS compatibility
    Fixed some issues

--- src/slabot/SLABot.java
+++ src/slabot/SLABot.java
@@ -9,6 +9,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -20,7 +21,7 @@ public class SLABot extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("mainSLA.fxml"));
-        
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/res/boticon.png")));
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
